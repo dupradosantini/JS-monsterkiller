927bac61d231eb904a98816ee12519835355c76c
Luis Eduardo <dupradosantini@opus-software.com.br>
2022-04-11 16:07:32 -0300

    Reset Game after its over

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -29,6 +29,9 @@ function endRound(){
     } else if(currentPlayerHealth <= 0 && currentMonsterHealth <= 0){
         alert("You have a draw!")
     }
+    if(currentMonsterHealth <= 0 || currentPlayerHealth <= 0){
+        reset();
+    }
 }
 
 function attackMonster(mode){
@@ -64,6 +67,12 @@ function healPlayerHandler(){
     increasePlayerHealth(healValue);
     currentPlayerHealth += healValue;
     endRound();
+}
+
+function reset(){
+    currentMonsterHealth = chosenMaxLife;
+    currentPlayerHealth = chosenMaxLife;
+    resetGame(chosenMaxLife);
 }
 
 attackBtn.addEventListener('click', attackHandler);

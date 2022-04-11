27c3f15ebcb2c3ab4109fd5b019d00dd96b9ac3d
Luis Eduardo <dupradosantini@opus-software.com.br>
2022-04-11 15:54:35 -0300

    Adding heal player functionality

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,23 +1,14 @@
 const ATTACK_VALUE = 10;
 const MONSTER_ATTACK_VALUE = 12;
 const STRONG_ATTACK_VALUE = 17;
+const HEAL_VALUE = 20;
 
 let chosenMaxLife = 100;
-adjustHealthBars(chosenMaxLife);
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
+adjustHealthBars(chosenMaxLife);
 
-function attackMonster(mode){
-    let maxDamage;
-    if(mode === "ATTACK"){
-        maxDamage = ATTACK_VALUE;
-    } else {
-        maxDamage = STRONG_ATTACK_VALUE;
-    }
-
-    const damage = dealMonsterDamage(maxDamage);
-    currentMonsterHealth = currentMonsterHealth - damage;
-
+function endRound(){
     const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
     currentPlayerHealth = currentPlayerHealth - playerDamage;
 
@@ -30,6 +21,20 @@ function attackMonster(mode){
     }
 }
 
+function attackMonster(mode){
+    let maxDamage;
+    if(mode === "ATTACK"){
+        maxDamage = ATTACK_VALUE;
+    } else {
+        maxDamage = STRONG_ATTACK_VALUE;
+    }
+
+    const damage = dealMonsterDamage(maxDamage);
+    currentMonsterHealth = currentMonsterHealth - damage;
+
+    endRound();
+}
+
 function attackHandler(){
     attackMonster("ATTACK");
 }
@@ -38,5 +43,19 @@ function strongAttackHandler(){
     attackMonster("STRONG_ATTACK")
 }
 
+function healPlayerHandler(){
+    let healValue;
+    if(currentPlayerHealth >= chosenMaxLife - HEAL_VALUE){
+        alert("You can't heal to more than your max health.");
+        healValue = chosenMaxLife - currentPlayerHealth;
+    } else {
+        healValue = HEAL_VALUE;
+    }
+    increasePlayerHealth(healValue);
+    currentPlayerHealth += healValue;
+    endRound();
+}
+
 attackBtn.addEventListener('click', attackHandler);
 strongAttackBtn.addEventListener('click', strongAttackHandler);
+healBtn.addEventListener('click', healPlayerHandler);

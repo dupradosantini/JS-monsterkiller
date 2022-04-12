75975b13d42aa652dd2b8d02132cd563c5d4c2bc
Luis Eduardo <dupradosantini@opus-software.com.br>
2022-04-12 15:32:49 -0300

    Adding try-catch to handle custom error.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -11,11 +11,27 @@ const LOG_EVENT_MONSTER_ATTACK = "MONSTER_ATTACK";
 const LOG_EVENT_PLAYER_HEAL = "PLAYER_HEAL";
 const LOG_EVENT_GAME_OVER = "GAME_OVER;"
 
-const enteredValue = prompt("Maximum life for you and the monster", "100");
-let chosenMaxLife = parseInt(enteredValue);
 let battleLog = [];
-if(isNaN(chosenMaxLife) || chosenMaxLife <= 0){
-    chosenMaxLife=100;
+
+function getMaxLifeValues(){
+    const enteredValue = prompt("Maximum health for you and the monster.", "100");
+    const parsedValue = parseInt(enteredValue);
+    if(isNaN(parsedValue) || parsedValue <= 0){
+        throw { message: "Invalid user input, should be a number" };
+    }
+    return parsedValue;
+}
+let chosenMaxLife;
+
+try {
+    chosenMaxLife = getMaxLifeValues();
+} catch (error) {
+    console.log(error);
+    chosenMaxLife = 100;
+    alert('Max Health Should be a Number!')
+} finally {
+    //used for cleanup, rethrowing an error for analytics
+    //mostly not needed, can be ommited.
 }
 
 let currentMonsterHealth = chosenMaxLife;
